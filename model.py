657b78e9e1b1f9473d5f47e0336c283266581983
Zheng Tang <oscartang1120@gmail.com>
2020-03-14 17:07:08 -0700

    cudnn?

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
-# torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.deterministic = True
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
